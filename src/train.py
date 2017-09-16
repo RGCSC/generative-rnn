88ec8c4764cf6f7d59231168a71c6b67651595f9
Jimmy Chen <jchen.rgss@gmail.com>
2017-09-16 18:59:21 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,12 +1,11 @@
 import tensorflow as tf
 import numpy as np
 import itertools
-import pandas
 import nltk
-import random
 
 ################## LOAD DATA ##################
 NUM_PROBS = 79
+MAX_SAMP = 1000
 solution_mapping = {1:34,2:33,3:50,4:50,5:44,6:50,7:50,8:50,9:50,10:42,
                     11:4,12:50,13:50,14:50,15:50,16:50,17:47,18:27,19:50,20:50,
                     21:50,22:50,23:50,24:50,25:50,26:50,27:50,28:50,29:50,30:50,
@@ -18,10 +17,10 @@ solution_mapping = {1:34,2:33,3:50,4:50,5:44,6:50,7:50,8:50,9:50,10:42,
 
 max_n = 0
 descs = []
-for i in range(NUM_PROBS):
+for i in range(1, NUM_PROBS+1):
     num = ''
     if i < 10:
-         num = '0' + str(i) 
+         num = '0' + str(i)
     else:
         num = str(i)
     state = nltk.word_tokenize(open('desc_' + num + '.txt', 'r').read().lower())
@@ -29,24 +28,25 @@ for i in range(NUM_PROBS):
       max_n = len(state)
     descs.append(state)
 
-vocab = set(descs)
+vocab = set(list(itertools.chain.from_iterable(descs)))
 
 word_indices = dict((s, i) for i, s in enumerate(vocab))
 indices_word = dict((i, s) for i, s in enumerate(vocab))
 
 code_mat = []
-code_ch = set()
-for i in range(NUM_PROBS):
+# code_ch = set()
+for i in range(1, NUM_PROBS+1):
     problem = []
     prob_num=''
     if i < 10:
         prob_num = '0' + str(i)
     else:
         prob_num = str(i)
-    for j in range(solution_mapping[i + 1]):
-        problem.append(open('code_' + prob_num + '_(' + str(j) + '.txt').read().lower())
+    for j in range(1, min(MAX_SAMP, solution_mapping[i]+1)):
+        problem.append(open('code_' + prob_num + '_(' + str(j) + ').txt').read().lower())
     code_mat.append(problem)
 
+# print(np.array(code_mat).shape)
 code_ch = set(list(''.join(list(itertools.chain.from_iterable(code_mat)))))
 # code_ch.remove(' ')
 print("Total chars: ", len(code_ch))
@@ -59,59 +59,75 @@ print("Total chars: ", len(code_ch))
 # 'Convert' chars to indices and indices to chars
 char_indices = dict((c, i) for i, c in enumerate(code_ch))
 indices_char = dict((i, c) for i, c in enumerate(code_ch))
-                  
+
 # cut code
 maxlen = 10
-step = 4
+step = 5
 # x, y
 seqs = []
 next_chars = []
+# print(code_mat)
 for row in code_mat:
-    temp1 = []
-    temp2 = []
-    for j in range(0, len(row) - maxlen, step):
-        temp1.append(row[j: j + maxlen])
-        temp2.append(row[j + maxlen])
-    seqs.append(temp1)
-    next_chars.append(temp2)
+	# print(row)
+	prob_t1 = []
+	prob_t2 = []
+	for sub in row:
+		temp1 = []
+		temp2 = []
+		for k in range(0, len(sub) - maxlen, step):
+			temp1.append(sub[k: k + maxlen])
+			temp2.append(sub[k+ maxlen])
+		prob_t1.append(temp1)
+		prob_t2.append(temp2)
+	seqs.append(prob_t1)
+	next_chars.append(prob_t2)
 print('num seqs:', len(seqs))
 
 # Vectorization of inputs, outputs
 max_row = 0
 for row in seqs:
-    if len(row) > max_row:
-        max_row = len(row)
+	for sub in row:
+		if len(sub) > max_row:
+			max_row = len(sub)
 
 # pad with spaces, get rid of jaggedness
 for row in seqs:
-    for i in range(max_row - len(row)):
-        row.append(' ')
+	for sub in row:
+		for i in range(maxlen - len(sub)):
+			sub.append(' ')
 arr = np.array(seqs)
 
 for row in next_chars:
-  for i in range(max_row - len(row)):
-    row.append(' ')
+	for sub in row:
+		for i in range(maxlen - len(sub)):
+			sub.append(' ')
 X_english = np.zeros((NUM_PROBS, max_n, len(vocab)), dtype=np.bool)
 
-X_code = np.zeros((NUM_PROBS, len(code_ch), maxlen, max_row), dtype=np.bool)
-y = np.zeros((NUM_PROBS, len(code_ch), max_row), dtype=np.bool)
+X_code = np.zeros((NUM_PROBS, 50, 1000, maxlen, len(code_ch)), dtype=np.bool)
+y = np.zeros((NUM_PROBS, 50, 1000, len(code_ch)), dtype=np.bool)
 
 for i in range(NUM_PROBS):
   for pos, word in enumerate(descs[i]):
     X_english[i, pos, word_indices[word]] = 1
-      
-      
+
+
+# print(np.array(next_chars).shape)
 for i in range(NUM_PROBS):
-  for j, seq in enumerate(seqs[i]):
-    for pos, char in enumerate(seq):
-      X_code[i, char_indices[char], pos, j] = 1
-    y[i, char_indices[next_chars[i][j]], j] = 1
+  for j in range(solution_mapping[i+1]):
+    for k in range(len(seqs[i][j])):
+      if len(seqs[i][j][k]) == 1: continue
+#       print(len(seqs[i][j][k]))
+      for l in range(maxlen):
+        X_code[i, j, k, l, char_indices[seqs[i][j][k][l]]] = 1
+      y[i, j, k, char_indices[next_chars[i][j][k]]] = 1
+
+
 # for i in range(len(seqs)):
 #     for j, seq in enumerate(seqs[i]):
 #         for pos, char in enumerate(seq):
 #             X_code[j, pos, char_indices[char], i] = 1
 #         y[j, char_indices[next_chars[i][j]], i] = 1
-      
+
 ###############################################
 
 
@@ -131,10 +147,10 @@ def sample(preds, temperature=1.0):
 english_model = tf.contrib.keras.models.Sequential()
 english_model.add(tf.contrib.keras.layers.Embedding(input_dim=len(vocab),
                                                     output_dim=128))
-                  
+
 code_model = tf.contrib.keras.models.Sequential()
-code_model.add(tf.contrib.keras.layer.LSTM(128, input_shape=(maxlen, len(code_ch), NUM_PROBS)))
-        
+code_model.add(tf.contrib.keras.layers.LSTM(128, input_shape=(NUM_PROBS, 50, 1000, maxlen, len(code_ch))))
+
 model = tf.contrib.keras.models.Sequential()
 model.add(tf.contrib.keras.layers.Merge([english_model, code_model]), mode='concat')
 model.add(tf.contrib.keras.layers.Dense(128, activation='softmax'))
@@ -152,7 +168,7 @@ model.fit(X_english, [X_code,y_code], batch_size=128,epochs=20,validation_split=
 #     print()
 #     print('-'*50)
 #     print('Iteration', iteration)
-#     model.fit(X, y, 
+#     model.fit(X, y,
 #             batch_size=128,
 #             epochs=1)
 
