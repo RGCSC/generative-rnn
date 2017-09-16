65ee74882360a553934fe3948b742da2e68439ff
Rudy Ariaz <rudyal147@gmail.com>
2017-09-16 15:42:11 -0400

    Stuff, should be done if everything's good

--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
-import pandas
 import numpy as np
+import itertools
+import pandas
 import nltk
 import random
 
-########### LOAD DATA ##############
+################## LOAD DATA ##################
 NUM_PROBS = 79
 solution_mapping = {1:34,2:33,3:50,4:50,5:44,6:50,7:50,8:50,9:50,10:42,
                     11:4,12:50,13:50,14:50,15:50,16:50,17:47,18:27,19:50,20:50,
@@ -15,6 +16,7 @@ solution_mapping = {1:34,2:33,3:50,4:50,5:44,6:50,7:50,8:50,9:50,10:42,
                     61:15,62:50,63:30,64:35,65:50,66:11,67:50,68:26,69:50,70:28,
                     71:50,72:46,73:50,74:44,75:34,76:50,77:50,78:50,79:49}
 
+max_n = 0
 descs = []
 for i in range(NUM_PROBS):
     num = ''
@@ -22,49 +24,41 @@ for i in range(NUM_PROBS):
          num = '0' + str(i) 
     else:
         num = str(i)
-    state = open('desc_'+num+'.txt', 'r').read().lower()
+    state = nltk.word_tokenize(open('desc_' + num + '.txt', 'r').read().lower())
+    if len(state) > max_n:
+      max_n = len(state)
     descs.append(state)
-  
-  
-      
-# Change constant below
+
+vocab = set(list(' '.join(descs)))
+
+word_indices = dict((s, i) for i, s in enumerate(vocab))
+indices_word = dict((i, s) for i, s in enumerate(vocab))
 
 code_mat = []
+code_ch = set()
 for i in range(NUM_PROBS):
     problem = []
     prob_num=''
     if i < 10:
-        prob_num = '0'+str(i)
+        prob_num = '0' + str(i)
     else:
-        prob_num=str(i)
-    for j in range(NUM_CODES):
-        code = open('code_'+prob_num+'_('+str(j)+'.txt').read().lower()
-        
+        prob_num = str(i)
+    for j in range(solution_mapping[i + 1]):
+        problem.append(open('code_' + prob_num + '_(' + str(j) + '.txt').read().lower())
+    code_mat.append(problem)
 
-####################################
-
-
-
-########## PREPROCESS DATA ##########
-#desc_t = nltk.word_tokenize(desc)
-#code1_t = nltk.word_tokenize(code1)
-#code2_t = nltk.word_tokenize(code2)
-#code3_t = nltk.word_tokenize(code3)
-#code4_t = nltk.word_tokenize(code4)    
-#code5_t = nltk.word_tokenize(code5)
-#code6_t = nltk.word_tokenize(code6)
-#code7_t = nltk.word_tokenize(code7)
-
-code = code1 + code2 + code3 + code4 + code5 + code6 + code7
-
-desc_ch = set(list(desc))
-code_ch = set(list(code))
-desc_ch.remove(' ')
+code_ch = set(list(''.join(list(itertools.chain.from_iterable(code_mat)))))
 code_ch.remove(' ')
-print('total chars:', len(desc_ch) + len(code_ch))
+print("Total chars: ", len(code_ch))
+###############################################
+
+
+
+############### PREPROCESS DATA ###############
+
 # 'Convert' chars to indices and indices to chars
-char_indices = dict((c, i) for i, c in enumerate(chars))
-indices_char = dict((i, c) for i, c in enumerate(chars))
+char_indices = dict((c, i) for i, c in enumerate(code_ch))
+indices_char = dict((i, c) for i, c in enumerate(code_ch))
                   
 # cut code
 maxlen = 10
@@ -76,8 +70,8 @@ for row in code_mat:
     temp1 = []
     temp2 = []
     for j in range(len(row) - maxlen, step):
-        temp1.append(row[j: j+maxlen])
-        temp2.append(row[j+maxlen])
+        temp1.append(row[j: j + maxlen])
+        temp2.append(row[j + maxlen])
     seqs.append(temp1)
     next_chars.append(temp2)
 print('num seqs:', len(seqs))
@@ -92,21 +86,28 @@ for row in seqs:
     for i in range(max_row - len(row)):
         row.append(' ')
 arr = np.array(seqs)
-X = np.zeros((arr.shape[1], maxlen, len(code_ch), NUM_CODES), dtype=np.bool)
-y = np.zeros((arr.shape[1], len(code_ch), NUM_CODES), dtype=np.bool)
 
+X_english = np.zeros((NUM_PROBS, max_n, len(vocab)), dtype=np.bool)
+
+X_code = np.zeros((arr.shape[1], maxlen, len(code_ch), NUM_PROBS), dtype=np.bool)
+y = np.zeros((arr.shape[1], len(code_ch), NUM_PROBS), dtype=np.bool)
+
+for i in range(NUM_PROBS):
+  for pos, word in enumerate(descs[i]):
+    X_english[i, pos, word_indices[word]] = 1
+      
+      
 for i in range(len(seqs)):
     for j, seq in enumerate(seqs[i]):
         for pos, char in enumerate(seq):
-            X[j, pos, char_indices[char], i] = 1
+            X_code[j, pos, char_indices[char], i] = 1
         y[j, char_indices[next_chars[j]], i] = 1
-
       
-####################################
+###############################################
 
 
 
-########### BUILD MODEL ###########
+################# BUILD MODEL #################
 # Sample character from probability distribution
 def sample(preds, temperature=1.0):
     # Convert
@@ -119,23 +120,24 @@ def sample(preds, temperature=1.0):
     return np.argmax(probs)
 
 english_model = tf.contrib.keras.models.Sequential()
-english_model.add(tf.contrib.keras.layers.Embedding(input_dim=english_vs,
+english_model.add(tf.contrib.keras.layers.Embedding(input_dim=len(vocab),
                                                     output_dim=128))
                   
 code_model = tf.contrib.keras.models.Sequential()
-code_model.add(tf.contrib.keras.layer.LSTM(128, input_shape=(maxlen, len(code_ch), NUM_CODES)))
+code_model.add(tf.contrib.keras.layer.LSTM(128, input_shape=(maxlen, len(code_ch), NUM_PROBS)))
         
 model = tf.contrib.keras.models.Sequential()
 model.add(tf.contrib.keras.layers.Merge([english_model, code_model]), mode='concat')
-model.add(tf.contrib.keras.layers.Dense(128, activation = 'softmax'))
-####################################
+model.add(tf.contrib.keras.layers.Dense(128, activation='softmax'))
+
+###############################################
 
 
 
-######### RUN MODEL, SAVE ########
+############# RUN AND SAVE MODEL ##############
 model.compile(loss='categorial_crossentropy', optimizer=tf.contrib.keras.optimizers.RMSprop(lr=0.01))
+model.fit(X_english, [X_code,y_code], batch_size=128,epochs=20,validation_split=0.2)
 
-code_model.fit(X, y, batch_size=128,epochs=20,validation_split=0.2)
 # train code model
 # for iteration in range(1, 60):
 #     print()
@@ -148,6 +150,4 @@ code_model.fit(X, y, batch_size=128,epochs=20,validation_split=0.2)
 #     problem = random.randint(0, NUM_PROBS-1)
 #     start_index = random.randint(0, len)
 
-
-#################################
-
+###############################################
